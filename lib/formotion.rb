1ac785de6be7df28c2311e13193fc99e2b14a64a
Clay Allsopp <clay.allsopp@gmail.com>
2012-06-24 23:30:44 -0400

    one-line require

--- lib/formotion.rb
+++ lib/formotion.rb
@@ -1,6 +1,4 @@
 require "formotion/version"
 require 'bubble-wrap/core'
 
-Dir.glob(File.join(File.dirname(__FILE__), 'formotion/**/*.rb')).each do |file|
-  BW.require file
-end
+BW.require File.expand_path('../formotion/**/*.rb', __FILE__)
