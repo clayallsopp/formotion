ab61ef283b2a507c3573b1ccb07ebef481868f11
Clay Allsopp <clay.allsopp@gmail.com>
2012-07-01 14:45:26 -0700

    add `depends_on` for files.

--- lib/formotion.rb
+++ lib/formotion.rb
@@ -1,4 +1,9 @@
 require "formotion/version"
 require 'bubble-wrap/core'
 
-BW.require File.expand_path('../formotion/**/*.rb', __FILE__)
+BW.require File.expand_path('../formotion/**/*.rb', __FILE__) do
+  ['form.rb', 'row.rb', 'section.rb'].each {|file|
+    file("lib/formotion/#{file}").depends_on 'lib/formotion/base.rb'
+  }
+  file("lib/formotion/form_controller.rb").depends_on 'lib/formotion/patch/ui_text_field.rb'
+end
