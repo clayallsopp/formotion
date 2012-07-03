01f0907b2bf200b6fb1ed613d87b113361bfde77
Clay Allsopp <clay.allsopp@gmail.com>
2012-07-02 20:52:52 -0700

    fix black-bar bug i introduced earlier.
    crap.

--- lib/formotion/form_controller.rb
+++ lib/formotion/form_controller.rb
@@ -6,15 +6,14 @@
 #
 #################
 module Formotion
-  class FormController < UIViewController
+  class FormController < UITableViewController
     attr_accessor :form
-    attr_reader :table_view
 
     # Initializes controller with a form
     # PARAMS form.is_a? [Hash, Formotion::Form]
     # RETURNS An instance of Formotion::FormController
     def initWithForm(form)
-      self.initWithNibName(nil, bundle: nil)
+      self.initWithStyle(UITableViewStyleGrouped)
       self.form = form
       self
     end
@@ -33,22 +32,9 @@ module Formotion
     def viewDidLoad
       super
 
-      # via https://gist.github.com/330916, could be wrong.
-      navBarHeight = self.navigationController && (self.navigationController.isNavigationBarHidden ? 0.0 : self.navigationController.navigationBar.bounds.size.height)
-      navBarHeight ||= 0
-      frame = self.view.frame
-      frame.size.height = frame.size.height - navBarHeight - tab_bar_height
-      self.view.frame = frame
-
       self.title = self.form.title
 
-      NSNotificationCenter.defaultCenter.addObserver(self, selector:'keyboardWillHideOrShow:', name:UIKeyboardWillHideNotification, object:nil);
-      NSNotificationCenter.defaultCenter.addObserver(self, selector:'keyboardWillHideOrShow:', name:UIKeyboardWillShowNotification, object:nil);
-
-      @table_view = UITableView.alloc.initWithFrame(self.view.bounds, style: UITableViewStyleGrouped)
-      self.view.addSubview @table_view
-
-      # Triggers this block when the enter key is pressed 
+      # Triggers this block when the enter key is pressed
       # while editing the last text field.
       @form.sections[-1] && @form.sections[-1].rows[-1].on_enter do |row|
         if row.text_field
@@ -61,47 +47,6 @@ module Formotion
       # @form as the datasource and delegate
       # and reloads the data.
       @form.controller = self
-    end
-
-    # This re-sizes + scrolls the tableview to account for the keyboard size.
-    # TODO: Test this on iPads, etc.
-    def keyboardWillHideOrShow(note)
-      last_note = @keyboard_state
-      @keyboard_state = note.name
-      if last_note == @keyboard_state
-        return
-      end
-
-      userInfo = note.userInfo
-      duration = userInfo[UIKeyboardAnimationDurationUserInfoKey].doubleValue
-      curve = userInfo[UIKeyboardAnimationCurveUserInfoKey].intValue
-      keyboardFrame = userInfo[UIKeyboardFrameEndUserInfoKey].CGRectValue
-
-      view_frame = self.view.frame;
-
-      if @keyboard_state == UIKeyboardWillHideNotification
-        view_frame.size.height = self.view.bounds.size.height
-      else
-        view_frame.size.height -= keyboardFrame.size.height
-        view_frame.size.height += tab_bar_height
-      end
-
-      UIView.beginAnimations(nil, context: nil)
-      UIView.setAnimationDuration(duration)
-      UIView.setAnimationDelay(0)
-      UIView.setAnimationCurve(curve)
-      UIView.setAnimationBeginsFromCurrentState(true)
-
-      @table_view.frame = view_frame
-      @form.active_row = @form.active_row
-
-      UIView.commitAnimations
-    end
-
-    private
-    def tab_bar_height
-      tabBarHeight = self.tabBarController && self.tabBarController.tabBar.bounds.size.height
-      tabBarHeight ||= 0
     end
   end
 end
