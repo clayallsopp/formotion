7b06bac6798d96340276907fec0cd130f7eeb575
Clay Allsopp <clay.allsopp@gmail.com>
2012-11-08 15:40:29 -0800

    :static rows are now subclasses of StringRow
    this means they also have access to :placeholder and :value

--- lib/formotion/row_type/static_row.rb
+++ lib/formotion/row_type/static_row.rb
@@ -1,6 +1,9 @@
 module Formotion
   module RowType
-    class StaticRow < Base
+    class StaticRow < StringRow
+      def after_build(cell)
+        self.row.text_field.enabled = false
+      end
     end
   end
 end
