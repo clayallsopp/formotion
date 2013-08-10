7088d40db2c095dbca6fc0ea0ebf5c1a30480b41
Stephan Toggweiler <stephan.toggweiler@me.com>
2013-08-10 18:44:17 +0200

    loading signaling

--- lib/formotion/row_type/web_view_row.rb
+++ lib/formotion/row_type/web_view_row.rb
@@ -9,22 +9,29 @@ module Formotion
 
       def set_page
         if row.value =~/^https?:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(:[a-zA-Z0-9]*)?\/?([a-zA-Z0-9\-\._\?\,\'\/\+&%\$#\=~])*$/
+          @loading = true
           req = NSURLRequest.requestWithURL(NSURL.URLWithString(row.value))
           @web_view.loadRequest(req)
-          p req
         else
           @web_view.loadHTMLString(row.value, baseURL:nil) if row.value
+          @loading = false
         end
       end
       
       def stringByEvaluatingJavaScriptFromString(script)
         @web_view.stringByEvaluatingJavaScriptFromString(script)
       end
+      
+      def loading
+        @loading
+      end
 
       def build_cell(cell)
         cell.selectionStyle = self.row.selection_style || UITableViewCellSelectionStyleBlue
-
+        
+        @loading = true
         @web_view = UIWebView.alloc.init
+        @web_view.delegate = self
         set_page
         
         observe(self.row, "value") do |old_value, new_value|
@@ -60,7 +67,17 @@ module Formotion
           return
         end
       end
+      
+      #def webView(web_view, didFailLoadWithError:error)
+      #end
 
+      #def webViewDidStartLoad(web_view)
+      #end
+
+      def webViewDidFinishLoad(web_view)
+        @loading = false
+      end
+      
     end
   end
 end
