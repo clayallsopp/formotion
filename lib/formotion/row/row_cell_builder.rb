cc93561d81c8b0e6694f331a08f0c607f6a08144
Clay Allsopp <clay.allsopp@gmail.com>
2013-10-02 09:41:23 -0700

    add observers for subtitle and title. fixes #157

--- lib/formotion/row/row_cell_builder.rb
+++ lib/formotion/row/row_cell_builder.rb
@@ -7,6 +7,7 @@
 #################
 module Formotion
   class RowCellBuilder
+    extend BW::KVO
 
     # PARAMS row.is_a? Formotion::Row
     # RETURNS [cell configured to that row, a UITextField for that row if applicable or nil]
@@ -16,8 +17,16 @@ module Formotion
       cell = UITableViewCell.alloc.initWithStyle(row.object.cell_style, reuseIdentifier:row.reuse_identifier)
 
       cell.accessoryType = cell.editingAccessoryType = UITableViewCellAccessoryNone
+
       cell.textLabel.text = row.title
+      observe(row, "title") do |old_value, new_value|
+        cell.textLabel.text = new_value
+      end
+
       cell.detailTextLabel.text = row.subtitle
+      observe(row, "subtitle") do |old_value, new_value|
+        cell.detailTextLabel.text = new_value
+      end
 
       edit_field = row.object.build_cell(cell)
       
