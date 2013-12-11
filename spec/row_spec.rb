f44c44c945ef2ffbc9922f382dfe7c0e34527cf9
Mark Rickert <mjar81@gmail.com>
2013-12-10 19:55:27 -0500

    Add tests for setting an image on the row

--- spec/row_spec.rb
+++ spec/row_spec.rb
@@ -13,6 +13,28 @@ describe "Rows" do
     r = Formotion::Row.new(hash)
     r.title.should == "TITLE"
     r.subtitle.should == "SUBTITLE"
+    r.image.should == nil
+  end
+
+  it "should sent an image on the cell with a String" do
+    hash = {title: "TITLE", subtitle: "SUBTITLE", image: "tags_row", type: :object, section: Formotion::Section.new({index: 0})}
+    r = Formotion::Row.new(hash)
+    r.index = 0
+    cell = r.make_cell
+
+    r.image.should == "tags_row"
+    cell.imageView.image.should.not == nil
+  end
+
+  it "should sent an image on the cell with a UIImage" do
+    image = UIImage.imageNamed("tags_row")
+    hash = {title: "TITLE", subtitle: "SUBTITLE", image: image, type: :object, section: Formotion::Section.new({index: 0})}
+    r = Formotion::Row.new(hash)
+    r.index = 0
+    cell = r.make_cell
+
+    r.image.should == image
+    cell.imageView.image.should == image
   end
 
   it "the question mark methods should work" do
